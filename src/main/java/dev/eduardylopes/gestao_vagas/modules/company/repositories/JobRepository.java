cf319635a9d7f511cafb8bca981545d285cf9445
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-26 16:26:31 -0300

    feat: create JobRepository

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,9 @@
+package dev.eduardylopes.gestao_vagas.modules.company.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.eduardylopes.gestao_vagas.modules.company.entities.JobEntity;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {}
