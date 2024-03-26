cc467a3fb5454fbfdaec8a808116d13853495fce
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-26 11:40:02 -0300

    feat: create CreateCandidateUserCase

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,24 @@
+package dev.eduardylopes.gestao_vagas.modules.candidate.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.eduardylopes.gestao_vagas.exceptions.UserFoundException;
+import dev.eduardylopes.gestao_vagas.modules.candidate.CandidateEntity;
+import dev.eduardylopes.gestao_vagas.modules.candidate.CandidateRepository;
+
+@Service
+public class CreateCandidateUseCase {
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+  
+  public CandidateEntity execute(CandidateEntity candidateEntity) {
+    this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent(user -> {
+      throw new UserFoundException();
+    });
+
+    return this.candidateRepository.save(candidateEntity);
+  }
+
+}
