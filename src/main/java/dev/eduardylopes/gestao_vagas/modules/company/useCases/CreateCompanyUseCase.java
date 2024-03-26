17f8a8ea2badd94823c784a75e388c62bcc8e98a
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-26 15:34:40 -0300

    feat: create CreateCompanyUseCase

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,24 @@
+package dev.eduardylopes.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dev.eduardylopes.gestao_vagas.exceptions.UserFoundException;
+import dev.eduardylopes.gestao_vagas.modules.company.entities.CompanyEntity;
+import dev.eduardylopes.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class CreateCompanyUseCase {
+
+  @Autowired
+  private CompanyRepository companyRepository;
+  
+  public CompanyEntity execute(CompanyEntity companyEntity) {
+
+    this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail()).ifPresent(user -> {
+      throw new UserFoundException();
+    });
+
+    return this.companyRepository.save(companyEntity);
+  }
+}
