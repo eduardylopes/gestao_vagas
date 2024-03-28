fd3180b931e6deff99f1113029682f45e40fd308
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 18:31:42 -0300

    feat: create ProfileCandidateUseCase

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -0,0 +1,31 @@
+package dev.eduardylopes.gestao_vagas.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import dev.eduardylopes.gestao_vagas.modules.candidate.CandidateRepository;
+import dev.eduardylopes.gestao_vagas.modules.candidate.dtos.ProfileCandidateResponseDTO;
+
+@Service
+public class ProfileCandidateUseCase {
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  public ProfileCandidateResponseDTO execute(UUID idCandidate) {
+    var candidate = this.candidateRepository.findById(idCandidate).orElseThrow(() -> {
+      throw new UsernameNotFoundException("User not found");
+    });
+
+    return ProfileCandidateResponseDTO.builder()
+        .id(candidate.getId())
+        .description(candidate.getDescription())
+        .email(candidate.getEmail())
+        .name(candidate.getName())
+        .username(candidate.getUsername())
+        .build();
+  }
+}
