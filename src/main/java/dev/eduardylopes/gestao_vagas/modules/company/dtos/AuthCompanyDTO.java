a29cb3c3d9c9f821c0052b0890c8634dd5fd4f13
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 14:31:23 -0300

    feat: create AuthCompanyDTO

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/company/dtos/AuthCompanyDTO.java
@@ -0,0 +1,13 @@
+package dev.eduardylopes.gestao_vagas.modules.company.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthCompanyDTO {
+
+  private String username;
+  private String password;
+
+}
