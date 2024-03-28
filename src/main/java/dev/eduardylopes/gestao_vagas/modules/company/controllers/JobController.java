d3fbf74ab7672aaba11211ef5ac6c8d9990db509
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 14:34:36 -0300

    feat: get companyId from request

--- src/main/java/dev/eduardylopes/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,5 +1,7 @@
 package dev.eduardylopes.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.eduardylopes.gestao_vagas.exceptions.CompanyNotFoundException;
+import dev.eduardylopes.gestao_vagas.modules.company.dtos.CreateJobDTO;
 import dev.eduardylopes.gestao_vagas.modules.company.entities.JobEntity;
 import dev.eduardylopes.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -19,14 +23,23 @@ public class JobController {
 
   @Autowired
   private CreateJobUseCase createJobUseCase;
-  
+
   @PostMapping
-  public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+  public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+
+    var companyId = request.getAttribute("company_id");
+
+    var jobEntity = JobEntity.builder()
+        .benefits(createJobDTO.getBenefits())
+        .description(createJobDTO.getDescription())
+        .level(createJobDTO.getLevel())
+        .companyId(UUID.fromString(companyId.toString()))
+        .build();
 
     try {
       var result = this.createJobUseCase.execute(jobEntity);
       return ResponseEntity.ok().body(result);
-      
+
     } catch (CompanyNotFoundException e) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
