45e8c357ff801ce9438dd90271657d82e29ac212
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 14:34:03 -0300

    feat: remove validation from JobEntity

--- src/main/java/dev/eduardylopes/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/company/entities/JobEntity.java
@@ -12,21 +12,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "jobs")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
-  
+
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
   private String description;
   private String benefits;
-
-  @NotBlank(message = "Esse campo é obrigatório")
   private String level;
 
   @ManyToOne
