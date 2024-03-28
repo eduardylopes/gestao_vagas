9d2f92b23431665073bbc5f3a5891022aa14a5b8
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 14:31:55 -0300

    feat: create AuthCompanyController

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -0,0 +1,31 @@
+package dev.eduardylopes.gestao_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.eduardylopes.gestao_vagas.modules.company.dtos.AuthCompanyDTO;
+import dev.eduardylopes.gestao_vagas.modules.company.useCases.AuthCompanyUseCase;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCompanyController {
+
+  @Autowired
+  AuthCompanyUseCase authCompanyUseCase;
+
+  @PostMapping("/company")
+  public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO) {
+
+    try {
+      var result = this.authCompanyUseCase.execute(authCompanyDTO);
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+  }
+}
