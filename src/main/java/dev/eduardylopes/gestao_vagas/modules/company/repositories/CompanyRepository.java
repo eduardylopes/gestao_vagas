28df18e07a6982020d624196102578b5a193ab39
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 14:32:30 -0300

    feat: add findByUsername to CompanyRepository

--- src/main/java/dev/eduardylopes/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -9,4 +9,6 @@ import dev.eduardylopes.gestao_vagas.modules.company.entities.CompanyEntity;
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
   Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+
+  Optional<CompanyEntity> findByUsername(String username);
 }
