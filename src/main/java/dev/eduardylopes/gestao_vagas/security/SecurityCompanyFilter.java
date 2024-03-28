ac5d959230b9965788938511ba7e70008f3b88b8
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 18:29:42 -0300

    refactor: change SecurityFilter to SecurityCompanyFilter

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/security/SecurityCompanyFilter.java
@@ -0,0 +1,59 @@
+package dev.eduardylopes.gestao_vagas.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import dev.eduardylopes.gestao_vagas.providers.JWTCompanyProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityCompanyFilter extends OncePerRequestFilter {
+
+  @Autowired
+  JWTCompanyProvider jwtCompanyProvider;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    if (request.getRequestURI().startsWith("/companies")) {
+      SecurityContextHolder.getContext().setAuthentication(null);
+      String token = request.getHeader("Authorization");
+
+      if (token != null) {
+        var decodedJwt = this.jwtCompanyProvider.validateToken(token);
+
+        if (decodedJwt == null) {
+          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+          return;
+        }
+
+        request.setAttribute("company_id", decodedJwt);
+
+        var roles = decodedJwt.getClaim("roles")
+            .asList(String.class)
+            .stream()
+            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
+            .toList();
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token,
+            null,
+            roles);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+      }
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+}
