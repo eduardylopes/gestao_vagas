c669f78a4be5d0f0de83d9558a6b330d29e37573
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 14:32:53 -0300

    feat: encoded company password

--- src/main/java/dev/eduardylopes/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,6 +1,7 @@
 package dev.eduardylopes.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import dev.eduardylopes.gestao_vagas.exceptions.UserFoundException;
@@ -12,12 +13,19 @@ public class CreateCompanyUseCase {
 
   @Autowired
   private CompanyRepository companyRepository;
-  
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public CompanyEntity execute(CompanyEntity companyEntity) {
 
-    this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail()).ifPresent(user -> {
-      throw new UserFoundException();
-    });
+    this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
+        .ifPresent(user -> {
+          throw new UserFoundException();
+        });
+
+    var encodedPassword = this.passwordEncoder.encode(companyEntity.getPassword());
+    companyEntity.setPassword(encodedPassword);
 
     return this.companyRepository.save(companyEntity);
   }
