51b8da1027c93f25958bf5dc60ac6a30578fa13c
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 15:18:55 -0300

    feat: create AuthCandidateResponseDTO

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/candidate/dtos/AuthCandidateResponseDTO.java
@@ -0,0 +1,15 @@
+package dev.eduardylopes.gestao_vagas.modules.candidate.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCandidateResponseDTO {
+
+  private String access_token;
+}
