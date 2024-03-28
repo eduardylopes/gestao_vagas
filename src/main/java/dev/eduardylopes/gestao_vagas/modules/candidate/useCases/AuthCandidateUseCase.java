2dd966471d2c4849ca3eee126cc56c8c8877af60
Eduardy Lopes <eduardylopes@gmail.com>
2024-03-28 15:19:29 -0300

    feat: create AuthCandidateUseCase

---
+++ src/main/java/dev/eduardylopes/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,55 @@
+package dev.eduardylopes.gestao_vagas.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import dev.eduardylopes.gestao_vagas.modules.candidate.CandidateRepository;
+import dev.eduardylopes.gestao_vagas.modules.candidate.dtos.AuthCandidateRequestDTO;
+import dev.eduardylopes.gestao_vagas.modules.candidate.dtos.AuthCandidateResponseDTO;
+
+@Service
+public class AuthCandidateUseCase {
+
+  @Value("${security.token.secret.candidate}")
+  private String secretKey;
+
+  @Autowired
+  CandidateRepository candidateRepository;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO)
+      throws AuthenticationException {
+    var candidate = this.candidateRepository.findByUsername(authCandidateRequestDTO.username()).orElseThrow(() -> {
+      throw new UsernameNotFoundException("Incorrect username or password");
+    });
+
+    var passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+    if (!passwordMatches) {
+      throw new AuthenticationException();
+    }
+
+    var token = JWT.create().withIssuer("eduardylopes")
+        .withSubject(candidate.getId().toString())
+        .withClaim("roles", Arrays.asList("candidate"))
+        .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+        .sign(Algorithm.HMAC256(secretKey));
+
+    return AuthCandidateResponseDTO.builder().access_token(token).build();
+
+  }
+}
